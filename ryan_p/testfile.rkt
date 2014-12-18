10fbd92716d9a53b52facf3748860eb122c7b3dc
rpottsoh@gmail.com <Eclipse123>
2014-12-18 12:23:11 -0500

    Just hacking
    Playing around some more with DrRacket.  Just trying out some of the
    easier beginning exercises and some of the examples from the readings.
    I will formalize this when I start working through the assignments.

--- ryan_p/testfile.rkt
+++ ryan_p/testfile.rkt
@@ -1,5 +1,12 @@
 #lang racket
 
+(define meters-to-feet
+  (λ (m)
+    (/ (* m 39.6) 12)))
+;(define (meters-to-feet m)
+;  (/ (* m 39.6) 12))
+
+
 (define (factorial n)
    (if (= n 1)
        n
@@ -30,6 +37,14 @@
 (define (<= x y)
   (or (< x y) (= x y)))
 
+(define (Smallest x y z)
+  (cond
+    [(= x y z) x]
+    [(and (<= x y) (<= x z)) x]
+    [(and (<= y x) (<= y z)) y]
+    [(and (<= z x) (<= z x)) z]
+    [else x]))
+
 (define (Largest x y z)
   (cond
     [(= x y z) x]
@@ -37,3 +52,21 @@
     [(and (>= y x) (>= y z)) y]
     [(and (>= z x) (>= z y)) z]
     [else x]))
+
+(define (sum-of-largest-squares x y z)
+  (cond
+    [(= x (Smallest x y z)) (sum-of-squares y z)]
+    [(= y (Smallest x y z)) (sum-of-squares x z)]
+    [(= z (Smallest x y z)) (sum-of-squares x y)]
+    [else 0]))
+
+(define max (λ ( a b) (if (> a b) a b)))
+(define max-sum (λ (a b) (max (+ a b) (- a b))))
+
+; if x=1 evaluates to 2
+(define λ1
+  (λ (x) (+ x x)))
+
+; error
+(define λ2
+  ((λ (x) (+ x x)) 2))

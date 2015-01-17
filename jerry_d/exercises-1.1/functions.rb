aacb8d4dc3fda5b96deda72f7dc7bc4d90e56386
Jerry D'Antonio <stumpjumper@gmail.com>
2015-01-17 10:25:45 -0500

    [Jerry] Exx 1.3 w/o assignment in Ruby

--- jerry_d/exercises-1.1/functions.rb
+++ jerry_d/exercises-1.1/functions.rb
@@ -1,5 +1,34 @@
 # Exercises: 1.1 The Elements of Programming
 
+def drop_the_smallest_by_reduction(*args)
+  min = args.min
+  found = false
+  result = args.reduce([]) do |memo, item|
+    if item == min && ! found
+      found = true
+    else
+      memo << item
+    end
+    memo
+  end
+  return result
+end
+
+def drop_the_smallest(*args)
+  drop_the_smallest_recursive(args.min, false, [], args)
+end
+
+def drop_the_smallest_recursive(min, found, accum, list)
+  if list.empty?
+    return accum
+  elsif list.first == min && ! found
+    drop_the_smallest_recursive(min, true, accum, list.drop(1))
+  else
+    accum << list.first
+    drop_the_smallest_recursive(min, found, accum, list.drop(1))
+  end
+end
+
 # Exercise 1.3
 #
 # Define a procedure that takes three numbers as arguments and
@@ -11,5 +40,15 @@ def sum_squares_of_two_biggest(a, b, c)
     a * a + c * c
   else
     a * a + b * b
+  end
+end
+
+# Exercise 1.3
+#
+# Define a procedure that takes three numbers as arguments and
+# returns the sum of the squares of the two larger numbers.
+def sum_squares_excluding_smallest(*args)
+  drop_the_smallest(*args).reduce(0) do |accum, n|
+    accum += n * n
   end
 end
--- jerry_d/exercises-1.1/functions_spec.rb
+++ jerry_d/exercises-1.1/functions_spec.rb
@@ -1,40 +1,85 @@
 require 'rspec'
 require_relative 'functions'
 
-context :sum_squares_of_two_biggest do
+context :drop_the_smallest do
+
+  it 'returns an empty array when given no arguments' do
+    result = drop_the_smallest
+    expect(result).to eq []
+  end
+
+  it 'returns an empty array when given one argument' do
+    result = drop_the_smallest(1)
+    expect(result).to eq []
+  end
+
+  it 'returns the the correct array when all elements are the same' do
+    result = drop_the_smallest(2, 2, 2)
+    expect(result).to eq [2, 2]
+  end
+
+  it 'returns the proper array when the smallest is first' do
+    result = drop_the_smallest(2, 4, 6)
+    expect(result).to include(4)
+    expect(result).to include(6)
+  end
+
+  it 'returns the proper array when the smallest is last' do
+    result = drop_the_smallest(6, 4, 2)
+    expect(result).to include(4)
+    expect(result).to include(6)
+  end
+
+  it 'returns the proper array when the smallest is in the middle' do
+    result = drop_the_smallest(4, 6, 2)
+    expect(result).to include(4)
+    expect(result).to include(6)
+  end
+
+  it 'only drops one element when multiple are the smallest value' do
+    result = drop_the_smallest(1, 2, 1, 3, 4)
+    expect(result.length).to eq 4
+    expect(result).to include(1)
+    expect(result).to include(2)
+    expect(result).to include(3)
+    expect(result).to include(4)
+  end
+end
+
+context :sum_squares_excluding_smallest do
 
   it 'returns 2 when given all arguments are 1' do
-    result = sum_squares_of_two_biggest(1, 1, 1)
+    result = sum_squares_excluding_smallest(1, 1, 1)
     expect(result).to eq 2
   end
 
   it 'returns 8 when given all arguments are 2' do
-    result = sum_squares_of_two_biggest(2, 2, 2)
+    result = sum_squares_excluding_smallest(2, 2, 2)
     expect(result).to eq 8
   end
 
   it 'returns 18 when given when given 3, 3, and a smaller number' do
-    result = sum_squares_of_two_biggest(3, 3, 2)
+    result = sum_squares_excluding_smallest(3, 3, 2)
     expect(result).to eq 18
   end
 
   it 'returns 13 when given 1, 2, and 3' do
-    result = sum_squares_of_two_biggest(1, 2, 3)
+    result = sum_squares_excluding_smallest(1, 2, 3)
     expect(result).to eq 13
   end
 
   it 'returns the proper result when the first arg is the smallest' do
-    result = sum_squares_of_two_biggest(2, 4, 6)
+    result = sum_squares_excluding_smallest(2, 4, 6)
     expect(result).to eq 52
   end
 
   it 'returns the proper result when the second arg is the smallest' do
-    result = sum_squares_of_two_biggest(4, 2, 6)
+    result = sum_squares_excluding_smallest(4, 2, 6)
     expect(result).to eq 52
   end
 
   it 'returns the proper result when the first arg is the smallest' do
-    result = sum_squares_of_two_biggest(6, 4, 2)
+    result = sum_squares_excluding_smallest(6, 4, 2)
     expect(result).to eq 52
   end
 end

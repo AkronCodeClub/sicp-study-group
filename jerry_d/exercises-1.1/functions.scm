d2d6afbbccd6cf2824e8be930b0ad0df467afdb5
Jerry D'Antonio <stumpjumper@gmail.com>
2015-01-18 11:52:48 -0500

    [Jerry] Ex. 1.8: cube root in Scheme.

--- jerry_d/exercises-1.1/functions.scm
+++ jerry_d/exercises-1.1/functions.scm
@@ -5,6 +5,15 @@
       (- x)
       x))
 
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (square x)
+  (* x x))
+
+(define (cube x)
+  (* x x x))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define (assertEqual expected actual message)
@@ -30,6 +39,7 @@
 
 (display "Suite: sum_squares_of_two_biggest")
 (newline)
+
 (assertEqual 2  (sum_squares_of_two_biggest 1 1 1) "returns 2 when given all arguments are 1")
 (assertEqual 8  (sum_squares_of_two_biggest 2 2 2) "returns 8 when given all arguments are 2")
 (assertEqual 18 (sum_squares_of_two_biggest 3 3 2) "returns 18 when given when given 3, 3, and a smaller number")
@@ -49,12 +59,6 @@
 (define (improve guess x)
   (average guess (/ x guess)))
 
-(define (average x y)
-  (/ (+ x y) 2))
-
-(define (square x)
-  (* x x))
-
 (define (good-enough? guess x)
   (< (abs (- (square guess) x)) 0.001))
 
@@ -63,6 +67,7 @@
 
 (display "Suite: sqrt")
 (newline)
+
 (assertWithin 1.4142156862745097 (sqrt 2) 0.001
               "returns ~1.414 for 2")
 (assertWithin 3.00009155413138   (sqrt 9) 0.001
@@ -74,3 +79,33 @@
 (assertWithin 1000.000369924366  (square (sqrt 1000)) 0.001
               "returns ~1000 for (square (sqrt 1000))")
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (cbrt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (cbrt-iter (improve guess x)
+                 x)))
+
+(define (improve guess x)
+  (/ (+ (/ x (square guess)) (* 2 guess)) 3))
+
+(define (good-enough? guess x)
+  (< (abs (- (cube guess) x)) 0.001))
+
+(define (cbrt x)
+  (cbrt-iter 1.0 x))
+
+(display "Suite: sum_squares_of_two_biggest")
+(newline)
+
+(assertWithin 1.2599210498948732 (cbrt 2) 0.001
+              "returns ~1.260 for 2")
+(assertWithin 2.080083823051904  (cbrt 9) 0.001
+              "returns ~2.080 for 9")
+(assertWithin 5.1551367354757724 (cbrt (+ 100 37)) 0.001
+              "returns ~5.155 for (+ 100 37)")
+(assertWithin 1.392849702964866  (cbrt (+ (cbrt 2) (cbrt 3))) 0.001
+              "returns ~1.393 for (+ (cbrt 2) (cbrt 3))")
+(assertWithin 1000.0000435797309 (cube (cbrt 1000)) 0.001
+              "returns ~1000 for (cube (cbrt 1000))")

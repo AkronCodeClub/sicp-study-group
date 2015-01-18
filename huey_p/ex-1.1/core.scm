9e1bc3478786f1e1d745369070b2b2722a58c527
Huey Petersen <eyston@gmail.com>
2015-01-18 00:50:39 -0500

    attempt to make emacs not use tabs in scheme

--- huey_p/ex-1.1/core.scm
+++ huey_p/ex-1.1/core.scm
@@ -6,19 +6,19 @@
 
 (define (sum-sq-largest-two a b c)
   (cond ((> a b c) (sum-sq a b))
-	((> a c b) (sum-sq a c))
-	((> b a c) (sum-sq b a))
-	((> b c a) (sum-sq b c))
-	((> c a b) (sum-sq c a))
-	((> c b a) (sum-sq c b))))
+        ((> a c b) (sum-sq a c))
+        ((> b a c) (sum-sq b a))
+        ((> b c a) (sum-sq b c))
+        ((> c a b) (sum-sq c a))
+        ((> c b a) (sum-sq c b))))
 
 (sum-sq-largest-two 3 9 6)
 
 
 (define (sum-sq-largest-two-v2 a b c)
   (cond ((and (>= a c) (>= b c)) (sum-sq a b))
-	((and (>= b a) (>= c a)) (sum-sq b c))
-	((and (>= c b) (>= a b)) (sum-sq a c))))
+        ((and (>= b a) (>= c a)) (sum-sq b c))
+        ((and (>= c b) (>= a b)) (sum-sq a c))))
 
 (sum-sq-largest-two-v2 3 9 6)
 

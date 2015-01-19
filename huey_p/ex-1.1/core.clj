53219ab7c602bf7cd52930c605dc6eb741c4dba6
Huey Petersen <eyston@gmail.com>
2015-01-18 22:53:14 -0500

    clojure version of 1.8

--- huey_p/ex-1.1/core.clj
+++ huey_p/ex-1.1/core.clj
@@ -32,3 +32,22 @@
     (apply sum-sq)))
 
 (sum-sq-largest-two'' 3 9 6 12 1 4)
+
+;;; 1.8
+
+(defn improve [guess x]
+  (double (/ (+ (/ x (* guess guess)) (* 2 guess)) 3)))
+
+(defn good-enough? [previous-guess guess]
+  (< (/ (java.lang.Math/abs (- previous-guess guess)) guess) 0.000001))
+
+(defn cube-root [x]
+  (loop [previous-guess 0
+         guess 1]
+    (if (good-enough? previous-guess guess)
+      guess
+      (recur guess (improve guess x)))))
+
+(cube-root 1e-13)
+
+(cube-root 27)

4ad7ddf71834c3b5884f0ae664d9c412ddd308d2
Huey Petersen <eyston@gmail.com>
2015-01-18 22:09:37 -0500

    exercise 1.8

--- huey_p/ex-1.1/core.scm
+++ huey_p/ex-1.1/core.scm
@@ -27,3 +27,32 @@
 (sum-sq-largest-two-v2 1 1 2)
 
 (sum-sq-largest-two-v2 1 2 2)
+
+;;; 1.8
+
+(define (square x)
+  (* x x))
+
+(define (cube x)
+  (* x x x))
+
+(define (good-enough? guess x)
+  (< (abs (- (cube guess) x)) 0.001))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (improve guess x)
+  (/ (+ (/ x (square guess)) (* 2 guess)) 3))
+
+
+(define (cube-root-iter guess x)
+  (if (good-enough? guess x)
+      (exact->inexact guess) ;; rational -> float
+      (cube-root-iter (improve guess x)
+                      x)))
+
+(define (cube-root x)
+  (cube-root-iter 1 x))
+
+(cube-root 27)

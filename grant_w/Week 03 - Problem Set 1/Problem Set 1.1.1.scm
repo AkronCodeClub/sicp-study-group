b531523f3b70bec64b425f74bc17610ef1ef1d21
Grant <grantwinney@gmail.com>
2015-01-25 22:13:46 -0500

    problem set 1, part 1

---
+++ grant_w/Week 03 - Problem Set 1/Problem Set 1.1.1.scm
@@ -0,0 +1,12 @@
+;;;
+; 1.1.1 - What are the values of the following Scheme expressions?
+;;;;;;;;;;;;;
+
+5
+; 5
+
+(+ 3 4)
+; 7
+
+(- (+ 3 4) (* 2 (+ 4 3)))
+; -7
---
+++ grant_w/Week 03 - Problem Set 1/Problem Set 1.1.4.scm
@@ -0,0 +1,18 @@
+;;;
+; 1.1.4 - For each statement below, indicate whether it is true or false.
+;;;;;;;;;;;;;
+
+; Names are self-evaluating.
+; false
+
+; Numbers are self-evaluating.
+; true
+
+; Every expression contained in parentheses is a combination.
+; false.. special forms like 'if' are in parentheses too, but are not combinations
+
+;(define 6 2) is a legal Scheme expression.
+; false, cannot redefine 6
+
+; define is a special form.
+; true

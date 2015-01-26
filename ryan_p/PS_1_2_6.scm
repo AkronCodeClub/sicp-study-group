999a5f9b0e97266d06a9498d11950c87e4b18277
rpottsoh@gmail.com <Eclipse123>
2015-01-26 09:47:12 -0500

    simplified bigger3
    My bigger3 is not as elegant as what I see everyone else checking in.

--- ryan_p/PS_1_2_6.scm
+++ ryan_p/PS_1_2_6.scm
@@ -7,7 +7,7 @@
     (if (> a b) a
       b)))
 
-(bigger2 30 3)
+(bigger2 30 3) ; 30
 
 (define bigger3
   (lambda (a b c)
@@ -15,4 +15,14 @@
           ((= (bigger2 b a) (bigger2 b c)) b)
           ((= (bigger2 c a) (bigger2 c b)) c))))
 
-(bigger3 10 1 1)
+(bigger3 10 1 1)  ; 10
+(bigger3 10 1 100); 100
+
+; simplified bigger3.  Don't know how I didn't see this originally.
+(define B3r
+  (lambda (a b c)
+    (bigger2 (bigger2 a b) c)))
+
+(B3r 10 1 1)  ; 10
+(B3r 10 1 100); 100
+
